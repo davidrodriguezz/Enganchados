329f2a0958a427e87097a34e882d49796f8e43e5
albgom21 <albgom21@ucm.es>
2022-11-24 11:51:32 +0100

    Minijuego Ruleta
    Co-Authored-By: davidrodriguezz <62748674+davidrodriguezz@users.noreply.github.com>
    Co-Authored-By: Alejao03 <61582507+Alejao03@users.noreply.github.com>

---
+++ ENGANCHADOS/Assets/PickerWheel/Scripts/Girar.cs
@@ -0,0 +1,17 @@
+﻿using EasyUI.PickerWheelUI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Girar : MonoBehaviour {
+    [SerializeField] private Button uiSpinButton;
+
+    [SerializeField] private PickerWheel pickerWheel;
+
+    private void Start()
+    {
+        uiSpinButton.onClick.AddListener(() =>
+        {
+            pickerWheel.Spin();
+        });
+    } 
+}
