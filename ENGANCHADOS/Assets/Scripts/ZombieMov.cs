b6ddfabf6aecf0627e3c2418cb198010e2a1e297
albgom21 <albgom21@ucm.es>
2022-12-04 12:47:24 +0100

    Minijuego zombies joya
    Co-Authored-By: Alejao03 <61582507+Alejao03@users.noreply.github.com>
    Co-Authored-By: davidrodriguezz <62748674+davidrodriguezz@users.noreply.github.com>

--- ENGANCHADOS/Assets/Scripts/ZombieMov.cs
+++ ENGANCHADOS/Assets/Scripts/ZombieMov.cs
@@ -8,23 +8,45 @@ public class ZombieMov : MonoBehaviour
     public int lifes = 14;
     public AudioSource death;
     public AudioSource shoot;
+    public SpriteRenderer blood;
+    public SpriteRenderer health;
     bool once = true;
+    bool bloodActivate = false;
+    float timer;
+    float healthPart;
+
+    private void Start()
+    {
+        healthPart = health.transform.localScale.x / lifes;
+    }
     void Update()
     {
         transform.Translate(new Vector2(2 * Time.deltaTime, 0));
+        if(bloodActivate)
+            timer += Time.deltaTime;
+        if(0.3f < timer)
+        {
+            blood.gameObject.SetActive(false);
+            bloodActivate = false;
+        }
     }
     private void OnMouseDown()
     {
         if (lifes > 0)
         {
+            timer = 0;
+            bloodActivate = true;
+            blood.gameObject.SetActive(true);
             shoot.Play();
+            health.transform.localScale -= new Vector3(healthPart,0,0);
             lifes--;
         }
-        else if( lifes <= 0 && once)
+        else if (lifes <= 0 && once)
         {
+            blood.gameObject.SetActive(false);
             death.Play();
             this.GetComponent<SpriteRenderer>().enabled = false;
-            Destroy(this.gameObject,0.5f);
+            Destroy(this.gameObject, 0.5f);
             once = false;
         }
     }
